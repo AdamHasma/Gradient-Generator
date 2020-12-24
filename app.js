b8d7cb49120d9e14a1fc204e31d6514d1bfe1c83
AdamHasma <friedensbaume@hotmail.de>
2020-12-24 18:26:06 +0100

    first publish

---
+++ app.js
@@ -0,0 +1,42 @@
+let layer1 = document.querySelector("div.layer1");
+let layer2 = document.querySelector("div.layer2");
+let button = document.querySelector("button");
+let slideLight = document.getElementById('slideLight');
+let slide = document.getElementById('slide');
+let sliderAmount = document.getElementById("sliderAmount");
+let sliderAmount2 = document.getElementById("sliderAmount2");
+
+slide.onmousemove = function() {
+  sliderAmount.innerHTML = this.value;
+}
+
+slideLight.onmousemove = function() {
+  sliderAmount2.innerHTML = this.value;
+}
+
+function makeColor() {
+  let h = Math.random() * 360;
+  let s = parseInt(slide.value);
+  let l =  parseInt(slideLight.value);
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function makeGradient() {
+  let c1 = makeColor();
+  let c2 = makeColor();
+  let angle = Math.random() * 360;
+  return `linear-gradient(${angle}deg, ${c1}, ${c2})`;
+}
+
+function changeBg() {
+  setTimeout(function() {
+    layer1.style.backgroundImage = makeGradient();
+    layer2.style.backgroundImage = makeGradient();
+  }, 0);
+}
+
+changeBg();
+
+button.addEventListener("click", function() {
+  changeBg();
+})
